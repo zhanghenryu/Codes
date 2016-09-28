051b1750a5f5a6d8079db80d3baa1316687df4ce
HenryChang <henry@cppor.com>
2016-09-28 10:43:09 +0800

    'test'

---
+++ src/com/cppor/codes/jspLearn/servlet/ServletLogin.java
@@ -0,0 +1,16 @@
+package com.cppor.codes.jspLearn.servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by ZhangHY on 2016/9/27.
+ */
+public class ServletLogin extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
